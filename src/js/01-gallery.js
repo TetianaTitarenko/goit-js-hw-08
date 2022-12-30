10627426117f0ffa6e5fd830e56e1f98fb4b1efa
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2022-12-30 17:17:59 +0000

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,41 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
 console.log(galleryItems);
 
-console.log(SimpleLightbox);
+const gallery = document.querySelector(".gallery")
+gallery.classList.add("gallery")
+
+function onClouse(event) {
+console.log(event)
+}
+
+gallery.addEventListener("click", onClikcImg)
+
+
+function makeItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt=""title="${description}" />
+        </a>
+  `;
+    })
+        .join(``);
+}
+
+const makeItems = makeItemsMarkup(galleryItems)
+
+gallery.innerHTML = makeItems
+
+const gallery1 = new SimpleLightbox('.gallery a', {captionDelay: 250});
+
+function onClikcImg(event) {
+        event.preventDefault()
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+}
