a0b8e022a75c865c90b2777792390a65961396eb
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2022-12-30 21:25:11 +0000

    add Video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,17 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-console.log(Player);
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlayTime, 1000));
+
+function onPlayTime(currentTime) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds))
+    }
+
+const timeStop = localStorage.getItem("videoplayer-current-time");
+// console.log(qwqewq)
+
+player.setCurrentTime(timeStop).then(function(seconds) {
+})
