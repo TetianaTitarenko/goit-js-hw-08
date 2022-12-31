209881bd1cff68f9e0d4a026d40a969db37b3d40
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2022-12-31 00:38:40 +0000

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector(`.feedback-form`);
 
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+form.addEventListener(`input`, throttle(onNameComent, 500));
+form.addEventListener(`submit`, onSubmit)
+
+const saveText = localStorage.getItem(STORAGE_KEY)
+    
+const formParse = JSON.parse(saveText)
+
+function onNameComent(event) {
+    formData[event.target.name] = event.target.value
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset()
+    console.log(formData)
+    localStorage.removeItem(STORAGE_KEY)    
+}
+ 
+    comebackText()
+
+function comebackText() {
+    if (saveText) {
+    form.elements.email.value = formParse.email || '';
+    form.elements.message.value = formParse.message || '';
+    }
+}
